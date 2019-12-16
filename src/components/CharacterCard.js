6f07e551e12cf4bcbca1db33c2eaf12bb5971cd2
bret-knight87 <bret.knight87@gmail.com>
2019-12-15 21:38:47 -0600

    data brought in, cards styled

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,24 @@
 import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardText,
+  Col,
+} from "reactstrap";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <Col xs="6" md="4" lg="3">
+      <Card className='m-1'>
+        <CardHeader tag='h6' className='text-center'>{props.name}</CardHeader>
+        <CardBody>
+          <CardText>Status: {props.status}</CardText>
+          <CardImg src={props.image} />
+        </CardBody>
+      </Card>
+    </Col>
+  );
 }
